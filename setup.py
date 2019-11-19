5091f2da00e4b2d04116783767bb0601fdb3089f
danilka <danilka_na@mail.ru>
2019-11-19 10:18:09 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name='Total Rounds',
-    version='1.0',
-    packages=find_packages(),
-    url='',
-    license='',
-    author='Almaz Rafikov',
-    author_email='madnessfox@yandex.ru',
-    description='',
-)
+import subprocess
+subprocess.call(["pip3", "install", "-r", "requirements.txt"])
